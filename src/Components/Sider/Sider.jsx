f458e8c92a2970360c8f1ed91e3075ddbd857281
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-05-20 07:29:54 +0600

    added Carousel section on jsx file done

---
+++ src/Components/Sider/Sider.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+
+const CarouselSection = () => {
+  return (
+
+<div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img
+          src="/Sider1.png"
+          className="w-full h-[300px] md:h-[400px] lg:h-[500px]  object-cover rounded-lg"
+        />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img
+    
+          src="/Sider2.png"
+          className="w-full h-[300px] md:h-[400px] lg:h-[500px] rounded-lg object-cover"
+        />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img 
+          src="/Sider3.png"
+          className="w-full h-[300px] md:h-[400px] lg:h-[500px] object-cover rounded-lg"
+        />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide4" className="carousel-item relative w-full">
+        <img
+          src="/Sider4.png"
+          className="w-full h-[300px] md:h-[400px] lg:h-[500px] object-cover rounded-lg"
+        />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+export default CarouselSection;
