5c1f6df7f3953e8ed2abb608cb83a9ff7e10ddb8
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-05-22 08:18:14 +0600

    single flower section done

--- src/Components/Flower/Flower.jsx
+++ src/Components/Flower/Flower.jsx
@@ -1,20 +1,21 @@
-import React, { use } from 'react';
-import SingleFlower from './SingleFlower';
+import React, { use } from "react";
+import SingleFlower from "./SingleFlower";
 
-const flowerPromise = fetch('/public/Flowers.json').then(res => res.json())
+const flowerPromise = fetch("/public/Flowers.json").then((res) => res.json());
 
 const Flower = () => {
+  const flowers = use(flowerPromise);
 
-    const flowers = use(flowerPromise)
-    console.log(flowers);
-
-    return (
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-20'>
-            {
-                flowers.map(flower => <SingleFlower flower={flower}/>)
-            }
-        </div>
-    );
+  return (
+    <div className="mb-80">
+        <h1 className="text-5xl font-bold text-center underline mt-60 mb-20">Own Flower </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-20">
+        {flowers.map((flower) => (
+          <SingleFlower key={flower.id} flower={flower} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Flower;
